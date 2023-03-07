3e9f01074237a4a25a8535dd88a5f712c06f264a
Наталія Яловега <109685236+NatalisDom@users.noreply.github.com>
2023-03-08 01:24:57 +0200

    commit

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,10 +1,33 @@
+// підключаємо компоненти з папок з компонентами тобто рендерім
 
+import { Profile } from 'components/profile/Profile';
+import { Statistics } from 'components/statistics/Statistics';
+import { FriendList } from 'components/friendList/FriendList';
+import { TransactionHistory } from 'components/transactionHistory/TransactionHistory';
+//підключаємо папки з  масивами даних стосовно користувача тощо
+import data from 'serverBase/data.json';
+import friends from 'serverBase/friends.json';
+import transactions from 'serverBase/transactions.json';
+import user from 'serverBase/user.json';
 
-
-
-
-
-
+//  експортуємо та рендерім у браузер
+export const App = () => {
+  return (
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
+  );
+}
 
 // export const App = () => {
 //   return (
--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,46 +1,41 @@
+import PropTypes from 'prop-types';
+import css from 'components/profile/Profile.module.css';
 
+// створюємо компонент реакт
 
+export const Profile = props => {
+  const { username, tag, location, avatar, stats } = props;
+  return (
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={avatar} alt={username} className={css.avatar} />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
 
+      <ul className={css.stats}>
+        <li>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{stats.followers}</span>
+        </li>
+        <li>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{stats.views}</span>
+        </li>
+        <li>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  )
+}
 
-
-
-
-
-
-{/* <div class="profile">
-  <div class="description">
-    <img
-      src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-      alt="User avatar"
-      class="avatar"
-    />
-    <p class="name">Petra Marica</p>
-    <p class="tag">@pmarica</p>
-    <p class="location">Salvador, Brasil</p>
-  </div>
-
-  <ul class="stats">
-    <li>
-      <span class="label">Followers</span>
-      <span class="quantity">1000</span>
-    </li>
-    <li>
-      <span class="label">Views</span>
-      <span class="quantity">2000</span>
-    </li>
-    <li>
-      <span class="label">Likes</span>
-      <span class="quantity">3000</span>
-    </li>
-  </ul>
-</div> */}
-
-// import user from 'path/to/user.json;
-
-// <Profile
-//   username={user.username}
-//   tag={user.tag}
-//   location={user.location}
-//   avatar={user.avatar}
-//   stats={user.stats}
-// />
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.objectOf(PropTypes.number)
+}
